2f0eff737dc5c4ebfc43a4b9a1f6de017ba17f4d
madhushalini970 <kethamadhushalini@gmail.com>
2026-05-03 21:13:26 +0530

    fifth commit

---
+++ shalini.py
@@ -0,0 +1,13 @@
+# Define two numbers
+a = 10
+b = 5
+
+# Perform calculations
+sum_result = a + b
+product_result = a * b
+
+# Print the results
+print(f"Number 1: {a}")
+print(f"Number 2: {b}")
+print(f"The sum is: {sum_result}")
+print(f"The product is: {product_result}")

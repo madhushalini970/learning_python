b7b1f250894a2a123586f4e273f016f775b85e91
madhushalini970 <kethamadhushalini@gmail.com>
2026-05-04 19:20:55 +0530

    seventh commit

---
+++ vikas.py
@@ -0,0 +1,8 @@
+try:
+    num = int(input("Enter a number: "))
+    if num % 2 == 0:
+        print("Even number")
+    else:
+        print("Odd number")
+except:
+    print("Please enter a valid number")
